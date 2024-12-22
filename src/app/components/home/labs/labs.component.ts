453906e2e597968fb091dcba601f63edbc721b9d
Padaipaaligal <padaipaaligal23@gmail.com>
2024-12-22 14:28:08 +0530

    #Add labs n what we do components

--- src/app/components/home/labs/labs.component.ts
+++ src/app/components/home/labs/labs.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './labs.component.scss'
 })
 export class LabsComponent {
-
+  labs = [
+    {
+      title: 'Core Lab',
+      description: 'Foundational knowledge and technical training for each technology field to ensure industry readiness.',
+      backgroundImg: '/assets/labs/core-lab.jpeg',
+      url: '',
+    },
+    {
+      title: 'CG Lab',
+      description: 'Collaborative activities like team projects, group discussions, and peer-learning exercises to foster teamwork and innovation.',
+      backgroundImg: '/assets/labs/cg-lab.png',
+      url: '',
+    },
+    {
+      title: 'Hub',
+      description: 'Real-world skill application through live projects, problem-solving tasks, and innovation challenges in a simulated professional environment.',
+      backgroundImg: '/assets/labs/hub.jpeg',
+      url: '',
+    }
+  ]
 }
--- src/app/components/home/what-we-do/what-we-do.component.ts
+++ src/app/components/home/what-we-do/what-we-do.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './what-we-do.component.scss'
 })
 export class WhatWeDoComponent {
-
+  timelineData = [
+    {
+      id: 1,
+      title: 'Corporate Experience & Collaborations',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.',
+      icon: '/assets/icons/collaboration.svg',
+    },
+    {
+      id: 2,
+      title: 'Emerging Technology Labs',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.',
+      icon: '/assets/icons/technology.svg',
+    },
+    {
+      id: 3,
+      title: 'Start-up Support Center',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.',
+      icon: '/assets/icons/support.svg',
+    },
+    {
+      id: 4,
+      title: 'Incubation & Innovation Center',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.',
+      icon: '/assets/icons/innovation.svg',
+    },
+  ];
 }
