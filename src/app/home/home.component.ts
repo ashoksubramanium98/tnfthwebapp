6e460fb59181fbd059c3cb49682f7783d5834167
Padaipaaligal <padaipaaligal23@gmail.com>
2024-12-22 06:47:35 +0530

    #Add about, labs and what-we-do component base setup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from '../components/home/about/about.component'
+import { BannerComponent } from '../components/home/banner/banner.component'
+import { LabsComponent } from '../components/home/labs/labs.component'
+import { PartnersComponent } from '../components/home/partners/partners.component'
+import { WhatWeDoComponent } from '../components/home/what-we-do/what-we-do.component'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AboutComponent, BannerComponent, LabsComponent, PartnersComponent, WhatWeDoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
