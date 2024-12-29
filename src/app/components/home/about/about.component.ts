87b252d4ba04b292c72df73921f4a29b214f3b3e
Padaipaaligal <padaipaaligal23@gmail.com>
2024-12-29 22:46:33 +0530

    #Add transitions for home page

--- src/app/components/home/about/about.component.ts
+++ src/app/components/home/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio >= 0.5) {
+            entry.target.classList.add('about__visible');
+          }  else {
+            entry.target.classList.remove('about__visible');
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
 
+    const sections = document.querySelectorAll('.about__content, .about__video');
+    sections.forEach(section => observer.observe(section));
+  }
 }
