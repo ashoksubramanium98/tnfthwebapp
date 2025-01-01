170d793ff823736748084002eaf9fcdae59a363c
Padaipaaligal <padaipaaligal23@gmail.com>
2025-01-01 08:45:00 +0530

    #Add internship form modal base setup

---
+++ src/app/components/internship/internship-form-modal/internship-form-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-internship-form-modal',
+  imports: [],
+  templateUrl: './internship-form-modal.component.html',
+  styleUrl: './internship-form-modal.component.scss'
+})
+export class InternshipFormModalComponent {
+
+}
