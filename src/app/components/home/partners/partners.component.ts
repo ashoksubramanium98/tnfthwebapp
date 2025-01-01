5eebf9c7a110a41bf808bfc86d3ba87741ceb035
Padaipaaligal <padaipaaligal23@gmail.com>
2025-01-01 07:40:43 +0530

    #Update internship page content

--- src/app/components/home/partners/partners.component.ts
+++ src/app/components/home/partners/partners.component.ts
@@ -8,17 +8,11 @@ import { Component, OnInit } from '@angular/core'
 })
 export class PartnersComponent implements OnInit {
   logoUrls = [
-    { id: 1, url: '/assets/partners/lightbox.png', alt: 'Light Box' }, { id: 2, url: '/assets/partners/globalBank.png', alt: 'Global Bank' },
-    { id: 3, url: '/assets/partners/boltShit.png', alt: 'Bolt Shit' }, { id: 4, url: '/assets/partners/nietzsche.png', alt: 'Nietzsche' },
-    { id: 5, url: '/assets/partners/featherDev.png', alt: 'Feather Dev' }, { id: 6, url: '/assets/partners/globalBank.png', alt: 'Global Bank' },
-    { id: 7, url: '/assets/partners/lightbox.png', alt: 'Light Box' }, { id: 8, url: '/assets/partners/spherule.png', alt: 'Spherule' } 
-  ]
-
-  logoUrls2 = [
-    { id: 9, url: '/assets/partners/lightbox.png', alt: 'Light Box' }, { id: 10, url: '/assets/partners/globalBank.png', alt: 'Global Bank' },
-    { id: 11, url: '/assets/partners/boltShit.png', alt: 'Bolt Shit' }, { id: 12, url: '/assets/partners/nietzsche.png', alt: 'Nietzsche' },
-    { id: 13, url: '/assets/partners/featherDev.png', alt: 'Feather Dev' }, { id: 14, url: '/assets/partners/globalBank.png', alt: 'Global Bank' },
-    { id: 15, url: '/assets/partners/lightbox.png', alt: 'Light Box' }, { id: 16, url: '/assets/partners/spherule.png', alt: 'Spherule' } 
+    { id: 1, url: '/assets/partners/ict.jpeg', alt: 'Global Bank' }, { id: 2, url: '/assets/partners/ueducate.jpeg', alt: 'Light Box' },
+    { id: 3, url: '/assets/partners/dell.jpeg', alt: 'Bolt Shit' }, { id: 4, url: '/assets/partners/cybertronium.jpeg', alt: 'Nietzsche' },
+    { id: 5, url: '/assets/partners/avl.jpeg', alt: 'Feather Dev' }, { id: 6, url: '/assets/partners/ict.jpeg', alt: 'Global Bank' },
+    { id: 7, url: '/assets/partners/ueducate.jpeg', alt: 'Light Box' }, { id: 8, url: '/assets/partners/dell.jpeg', alt: 'Bolt Shit' },
+    { id: 9, url: '/assets/partners/cybertronium.jpeg', alt: 'Nietzsche' }, { id: 10, url: '/assets/partners/avl.jpeg', alt: 'Feather Dev' }
   ]
 
   scrollPosition = 0
@@ -30,7 +24,6 @@ export class PartnersComponent implements OnInit {
   }
 
   startInfiniteLoop() {
-    this.logoUrls = [...this.logoUrls, ...this.logoUrls2]
     this.intervalId = setInterval(() => {
       this.scrollPosition += this.speed
       if (this.scrollPosition >= (this.logoUrls.length * 200 / 4)) {
--- src/app/internship/internship.component.ts
+++ src/app/internship/internship.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { InternshipHomeComponent } from '../components/internship/internship-home/internship-home.component';
+import { InternshipModulesComponent } from '../components/internship/internship-modules/internship-modules.component';
+import { InternshipBannerComponent } from '../components/internship/internship-banner/internship-banner.component';
 
 @Component({
   selector: 'app-internship',
-  imports: [],
+  imports: [InternshipHomeComponent, InternshipModulesComponent, InternshipBannerComponent],
   templateUrl: './internship.component.html',
   styleUrl: './internship.component.scss'
 })
